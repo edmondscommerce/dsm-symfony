182595462c3bd1ae413cbc099d9df641f126017d
Ross Mitchell <ross@edmondscommerce.co.uk>
2018-05-08 14:26:35 +0100

    Adding autoloading and a basic bundle file

---
+++ src/DsmApiPlatformBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category EdmondsCommerce
+ * @package  EdmondsCommerce_
+ * @author   Ross Mitchell <ross@edmondscommerce.co.uk>
+ */
+
+namespace EdmondsCommerce\DsmApiPlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DsmApiPlatformBundle extends Bundle
+{
+
+}
